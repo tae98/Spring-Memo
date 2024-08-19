1325387af39c9b49669030f7ff5c8b0f2e421e8e
taehyunkim <98rlaxogus@gmail.com>
2024-08-19 15:47:26 +0900

    ioc, di설계 적용

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -9,10 +9,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 public class MemoService {
-    private final JdbcTemplate jdbcTemplate;
+
+    private final MemoRepository memoRepository;
 
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoRepository = new MemoRepository(jdbcTemplate);
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
@@ -20,7 +21,6 @@ public class MemoService {
         Memo memo = new Memo(requestDto);
 
         // DB 저장
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         Memo saveMemo = memoRepository.save(memo);
 
 
@@ -31,12 +31,10 @@ public class MemoService {
     }
 
     public List<MemoResponseDto> getMemos() {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         return memoRepository.findAll();
     }
 
     public Long updateMemo( Long id, MemoRequestDto requestDto) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
@@ -49,7 +47,6 @@ public class MemoService {
     }
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
